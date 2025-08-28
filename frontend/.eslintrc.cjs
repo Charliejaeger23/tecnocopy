26e1ee6b9e9fc0446af214d9cd249974b3c16e5a
Charliejaeger23 <carlosqram@gmail.com>
2025-08-28 12:35:28 -0400

    fix(frontend): switch to .eslintrc.cjs to avoid eslint/js export issue

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,21 @@
+/* frontend/.eslintrc.cjs */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['react'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
